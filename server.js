b1d98794f721c369a0d746cea7c33d73ad67707a
President <aaron.m.wilson@uconn.edu>
2022-09-08 16:51:09 -0400

    added html and api routes to server file

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+// requiring npm modules
 const express = require('express');
 const path = require('path');
 
+// setting up server with express
 const app = express();
 const PORT = 3001;
 
+// setting up variables for routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// setting up get routes 
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes)
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// setting up server to listen upon npm start
 app.listen(PORT, () => {
     console.log(`App now listening`)
 })
